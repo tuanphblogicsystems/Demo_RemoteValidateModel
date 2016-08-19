118d53a1069fd6134a8c9e2f1823895da07d5a5b
tuancoi_blogic <tuancoi250689@gmail.com>
2016-08-19 12:44:38 +0700

    Add project files.

---
+++ Demo_RemoteValidateModel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Demo_RemoteValidateModel.Startup))]
+namespace Demo_RemoteValidateModel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
